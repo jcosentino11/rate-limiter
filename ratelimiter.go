7e9262da54b79032208d025884d8a211fd6d294f
Joseph Cosentino <jcosentino11@gmail.com>
2021-05-13 15:48:11 -0400

    simple impl

--- ratelimiter.go
+++ ratelimiter.go
@@ -1,5 +1,47 @@
 package ratelimiter
 
-func HelloWorld() string {
-	return "Hi there"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type RateLimiter struct {
+	rdb *redis.Client
+	opt *Options
+}
+
+type Options struct {
+	RequestsPerSecond int64
+}
+
+func NewDefaultOptions() *Options {
+	return &Options{
+		RequestsPerSecond: 100,
+	}
+}
+
+func NewRateLimiter(rdb *redis.Client, opt *Options) *RateLimiter {
+	return &RateLimiter{
+		rdb: rdb,
+		opt: opt,
+	}
+}
+
+func (rl *RateLimiter) Aquire(ctx context.Context, key string) (bool, error) {
+	// bin key by second
+	currSecond := fmt.Sprintf("%d", time.Now().Unix())
+	timeBoxedKey := key + ":" + currSecond
+
+	pipe := rl.rdb.Pipeline()
+	incr := pipe.Incr(ctx, timeBoxedKey)
+	pipe.Expire(ctx, key, 2*time.Second)
+
+	if _, err := pipe.Exec(ctx); err != nil {
+		return false, err
+	}
+
+	return incr.Val() <= rl.opt.RequestsPerSecond, nil
 }
--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -1,13 +1,46 @@
 package ratelimiter
 
 import (
+	"context"
 	"testing"
 	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/go-redis/redis/v8"
 )
 
-func TestHello(t *testing.T) {
-	if HelloWorld() == "" {
-		t.Fail()
+func newRedisClient(s *miniredis.Miniredis) *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr: s.Addr(),
+	})
+}
+
+func newMockRedisServer() *miniredis.Miniredis {
+	s, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
+
+func TestAquire(t *testing.T) {
+	s := newMockRedisServer()
+	defer s.Close()
+
+	rdb := newRedisClient(s)
+	defer rdb.Close()
+
+	rl := NewRateLimiter(rdb, &Options{
+		RequestsPerSecond: 1,
+	})
+
+	allowed, err := rl.Aquire(context.TODO(), "key1")
+	if err != nil {
+		panic(err)
+	}
+
+	if !allowed {
+		t.Fatal("Unable to aquire rate limit")
 	}
 }
 
